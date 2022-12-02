bfae8bdad7478b656b4e52805a038b7749fa47fe
tonyskyday <tony@tonyscida.com>
2022-12-02 13:45:57 -0500

    Initial Commit

---
+++ Theme Buggy/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Theme Buggy
+//
+//  Created by Tony Scida on 12/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Theme Buggy/Theme_BuggyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Theme_BuggyApp.swift
+//  Theme Buggy
+//
+//  Created by Tony Scida on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Theme_BuggyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
